5459714aec29815b2d124c70aa9b7388f0c730e8
minalDev-git <mishalminal21@gmail.com>
2024-07-18 18:17:28 +0500

    Made it Responsive

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let input = document.getElementById("inputBox");
+const input = document.getElementById("inputBox");
 let buttons = document.querySelectorAll("button");
 
+function moveCursorToEnd() {
+    const valueLength = input.value.length;
+    input.setSelectionRange(valueLength, valueLength);
+}
+  // Event listener to move cursor to end on input focus
+input.addEventListener('focus', moveCursorToEnd);
+
+let equalsTOBtn = document.getElementsByClassName("equalBtn");
+// Event listener to hide cursor when input field is empty
 let string = "";
 let ans = "";
 //Creates an array from an iterable object
@@ -8,24 +17,47 @@ let arr = Array.from(buttons);
 arr.forEach((button)=>{
     button.addEventListener("click",(e)=>{
         if (e.target.innerHTML == '=') {
-            //eval function Evaluates valid JavaScript code and executes it.
-            ans = eval(string);
-            //input ke tag mei value dikhe gi
-            input.value = ans;
-            string = ans; 
-            // string = "";
+            if (input.value == "") {
+                equalsTOBtn.disabled = true;
+            }
+            else{
+                equalsTOBtn.disabled = false;
+                //eval function Evaluates valid JavaScript code and executes it.
+                ans = eval(string);
+                //input ke tag mei value dikhe gi
+                input.value = ans;
+                string = ans; 
+                moveCursorToEnd();
+                input.style.caretColor = 'transparent';
+                // string = "";
+            }
         }
         else if (e.target.innerHTML == 'AC'){
             input.value = "";
+            input.style.caretColor = 'transparent';
             string = "";
+            ans = "";
         }
         else if (e.target.innerHTML == 'DEL'){
             string = string.substring(0,string.length-1);
             input.value = string;
+            if (input.value === '') {
+                input.style.caretColor = 'transparent';
+            }
+            input.focus();
         }
         else{
-            string += e.target.innerHTML;
-            input.value = string;
+            if (ans == undefined || ans == "Infinity") {
+                ans = "";
+                string = "";
+                input.value = "";
+            }
+            else{
+                string += e.target.innerHTML;
+                input.value = string;
+                input.style.caretColor = '';
+            }
         }
+        input.focus();
     });
 });
